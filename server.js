bd7241807b7d84b0fff0452284216a1838811dee
Radhika Gulati <radhigulati@gmail.com>
2021-01-05 12:47:37 -0800

    add netlify

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict';
+// 'use strict';
 
-const path = require('path');
-const express = require('express');
+// const path = require('path');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-const port = process.env.PORT || 3338;
+// const port = process.env.PORT || 3338;
 
-app.use('/', express.static(path.join(__dirname, 'html')));
+// app.use('/', express.static(path.join(__dirname, 'html')));
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
+// app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
