3779ad69543b59b4ca3f6919621e081813077c3f
Radhika Gulati <radhigulati@gmail.com>
2021-01-13 11:06:27 -0800

    made changes to todolist

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filter-todo');
+// const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('change', filterTodo);
+// filterOption.addEventListener('change', filterTodo);
 
 //Functions
 function addTodo(event){
+//todoButton.addEventListener('click', function(event){
     //console.log(event.target);
     // prevent form from submitting
     event.preventDefault();
@@ -36,18 +37,19 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     // clear todo input value
     todoInput.value = "";
-}
+};
 
 function deleteCheck(e){
     //console.log(e.target);
     const item = e.target;
     if(item.classList[0] === 'delete-btn'){
         const todo = item.parentElement;
+        todo.remove();
         // animation
-        todo.classList.add("fall");
-        todo.addEventListener("transitionend", function() {
-            todo.remove();
-        });
+        // todo.classList.add("fall");
+        // todo.addEventListener("transitionend", function() {
+        //     todo.remove();
+        // });
     }
     // check mark
     if(item.classList[0] === "complete-btn"){
@@ -56,28 +58,28 @@ function deleteCheck(e){
     }
 }
 
-function filterTodo(e) {
-    const todos = todoList.childNodes;
-    todos.forEach(function(todo) {
-      switch (e.target.value) {
-        case "all":
-          todo.style.display = "flex";
-          break;
-        case "completed":
-          if (todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-        case "uncompleted":
-          if (!todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-      }
-    });
-  }
+// function filterTodo(e) {
+//     const todos = todoList.childNodes;
+//     todos.forEach(function(todo) {
+//       switch (e.target.value) {
+//         case "all":
+//           todo.style.display = "flex";
+//           break;
+//         case "completed":
+//           if (todo.classList.contains("completed")) {
+//             todo.style.display = "flex";
+//           } else {
+//             todo.style.display = "none";
+//           }
+//           break;
+//         case "uncompleted":
+//           if (!todo.classList.contains("completed")) {
+//             todo.style.display = "flex";
+//           } else {
+//             todo.style.display = "none";
+//           }
+//       }
+//     });
+//   }
 
 
